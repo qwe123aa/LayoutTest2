e8677523e656891bd9ed91db9c96b6e8ded0709e
thdud__ <love05061114@gmail.com>
2023-05-30 19:39:04 +0900

    클래스에서 버튼위젯 생성 및 추가

--- app/src/main/java/kr/hs/emirim/limsoyeong/layouttest2/MainActivity.kt
+++ app/src/main/java/kr/hs/emirim/limsoyeong/layouttest2/MainActivity.kt
@@ -1,11 +1,31 @@
 package kr.hs.emirim.limsoyeong.layouttest2
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.dir5_2)
+
+        val linear1 = LinearLayout(this)
+        linear1.orientation = LinearLayout.VERTICAL
+        linear1.setBackgroundColor(Color.GREEN)
+
+        val params = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT)
+        setContentView(linear1, params)
+
+        var btn = Button(this)
+        btn.text = "클릭해보세요"
+        btn.setBackgroundColor(Color.rgb(255, 0, 255))
+        linear1.addView(btn)
+
+        btn.setOnClickListener {
+            Toast.makeText(applicationContext, "클래스에서 생성된 버튼입니다 ", Toast.LENGTH_LONG).show()
+        }
+
     }
 }
