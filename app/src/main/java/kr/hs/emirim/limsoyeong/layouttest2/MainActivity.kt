1038542ec73f282749dae61580362b51377157ab
thdud__ <love05061114@gmail.com>
2023-05-30 19:13:11 +0900

    Initial commit

---
+++ app/src/main/java/kr/hs/emirim/limsoyeong/layouttest2/MainActivity.kt
@@ -0,0 +1,11 @@
+package kr.hs.emirim.limsoyeong.layouttest2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.dir5_2)
+    }
+}
